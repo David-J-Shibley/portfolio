31ae0816f0d2cacb1a7d9dc0c4a6e57e916132c6
david shibley <david.shibley@contentful.com>
2025-03-09 22:27:58 -0600

    updating vite config to define ENVs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
     host: "::",
     port: 8080,
   },
@@ -19,4 +22,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  define: {
+    'process.env.VITE_EMAIL_TEMPLATE_ID': JSON.stringify(env.VITE_EMAIL_TEMPLATE_ID),
+    'process.env.VITE_EMAIL_PUBLIC_KEY': JSON.stringify(env.VITE_EMAIL_PUBLIC_KEY),
+    'process.env.VITE_EMAIL_SERVICE_ID': JSON.stringify(env.VITE_EMAIL_SERVICE_ID),
+}
+}});
