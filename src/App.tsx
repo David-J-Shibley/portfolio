a373d6784aaac343276e212915e95a98ddc747ed
david shibley <david.shibley@contentful.com>
2025-03-10 22:27:41 -0600

    fixing build take two

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages";
+import Index from "./pages/index.tsx";
 import NotFound from "./pages/NotFound";
 import Checkers from "./projects/checkers/pages";
 import Lobby from "./projects/chess/components/Lobby.tsx";
